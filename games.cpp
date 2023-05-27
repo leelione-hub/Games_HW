3b4f491f4342ba67d6641f6c501299dbb5d0c301
leelione-hub <lingaijie1997@outlook.com>
2023-05-27 17:06:01 +0900

    feat:windows 本地配置Games101 环境提交 Eigen+opencv

---
+++ games.cpp
@@ -0,0 +1,17 @@
+﻿#include<cmath>
+#include<eigen3/Eigen/Core>
+#include<eigen3/Eigen/Dense>
+#include<iostream>
+#include<opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+    cout << "Hello world!" << endl;
+    Mat imageMat = imread("./resources/textures/1000.png");
+    namedWindow("figure1", WINDOW_AUTOSIZE);
+    imshow("figure1", imageMat);
+    waitKey(0);
+    return 0;
+}
